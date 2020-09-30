8e78665e32ae033f83c07bafd5e824c314d89146
BCADOMAIN\u068799 <rendy_hartono@bca.co.id>
2020-09-30 13:30:13 +0700

    Iniatial Dev

---
+++ sendiri/src/main/java/com/belajar/sendiri/SendiriApplication.java
@@ -0,0 +1,13 @@
+package com.belajar.sendiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendiriApplication.class, args);
+	}
+
+}
---
+++ sendiri/src/test/java/com/belajar/sendiri/SendiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajar.sendiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
